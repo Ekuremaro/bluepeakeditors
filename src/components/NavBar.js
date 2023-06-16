d85460cb31b3694eeabe90e67eb7d8ccca0f2917
ekuremaro <engrekuremaro@gmail.com>
2023-06-16 20:05:59 +0100

    v1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,14 +21,14 @@ const NavBar = () => {
           >
             <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
               <div className="flex items-center justify-between w-full md:w-auto">
-                <a href="/" className='md:mr-5'>
+                <Link to="/" className='md:mr-5'>
                   <span className="sr-only">Workflow</span>
                   <img
                     alt="Workflow"
                     className="h-8 w-auto sm:h-10"
                     src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                   />
-                </a>
+                </Link>
                 <p className=" text-blue-700 font-semibold text-xl font-peralta">
                   Blue Peak Editors
                 </p>
@@ -42,13 +42,13 @@ const NavBar = () => {
             </div>
             <div className="hidden md:block md:ml-10 md:pr-0 md:space-x-4 lg:space-x-8">
               {navigation.map((item) => (
-                <a
+                <Link
                   key={item.name}
-                  href={item.href}
+                  to={item.href}
                   className="font-medium text-gray-500 hover:text-gray-900"
                 >
                   {item.name}
-                </a>
+                </Link>
               ))}
               <Link to="/pricing" className="font-medium btn btn-primary ">
                 Order Now
@@ -89,13 +89,13 @@ const NavBar = () => {
               </div>
               <div className="px-2 pt-2 pb-3 space-y-1">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
-                    href={item.href}
+                    to={item.href}
                     className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <Link to="/pricing"
